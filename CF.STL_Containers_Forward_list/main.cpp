c0efae3b91d1d28f6ea7414167b67622db4c79e2
Alan Sampson <alansamps@gmail.com>
2021-03-24 10:27:48 -0700

    Initial Commit

---
+++ CF.STL_Containers_Forward_list/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CF.STL_Containers_Forward_list
+//
+//  Created by Alan Sampson on 3/24/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
